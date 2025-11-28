67214132be7a55a2f64353da4cacc9af79b960a2
UltimateQuack <patoflamejantetv@gmail.com>
2025-11-28 16:01:16 -0300

    Rollbacking due Wayback Machine servers getting exhausted very quickly.
    ```
    + wget -nc https://web.archive.org/web/20250614092902/http://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.gz
    --2025-11-28 15:55:32--  https://web.archive.org/web/20250614092902/http://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.gz
    Resolving web.archive.org (web.archive.org)... 207.241.237.3
    Connecting to web.archive.org (web.archive.org)|207.241.237.3|:443... connected.
    HTTP request sent, awaiting response... 302 FOUND
    Location: https://web.archive.org/save/_embed/http://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.gz [following]
    --2025-11-28 15:55:33--  https://web.archive.org/save/_embed/http://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.gz
    Reusing existing connection to web.archive.org:443.
    HTTP request sent, awaiting response... 302 FOUND
    Location: https://web.archive.org/web/20251128185534/http://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.gz [following]
    --2025-11-28 15:55:46--  https://web.archive.org/web/20251128185534/http://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.gz
    Reusing existing connection to web.archive.org:443.
    HTTP request sent, awaiting response... 302 FOUND
    Location: https://web.archive.org/save/_embed/http://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.gz [following]
    --2025-11-28 15:55:46--  https://web.archive.org/save/_embed/http://ftp.gnu.org/gnu/gcc/gcc-11.4.0/gcc-11.4.0.tar.gz
    Reusing existing connection to web.archive.org:443.
    HTTP request sent, awaiting response... 429 TOO MANY REQUESTS
    2025-11-28 15:55:49 ERROR 429: TOO MANY REQUESTS.
    
    make[1]: *** [Makefile:71: tools] Error 8
    make[1]: Leaving directory '/home/pato/cavOS/src/kernel'
    make: *** [Makefile:71: tools] Error 2
    pato@Principal:~/cavOS$
    ```

--- tools/toolchain/get_autotools.sh
+++ tools/toolchain/get_autotools.sh
@@ -17,43 +17,17 @@ mkdir -p tmp_get_autotools
 cd tmp_get_autotools
 
 build_package() {
-    package_name=$1
-    version=$2
-    install_dir=$3
+	package_name=$1
+	version=$2
+	install_dir=$3
 
-    file="$package_name-$version.tar.gz"
-    url="https://ftp.gnu.org/gnu/$package_name/$file"
-
-    echo "-> Downloading $file from $url"
-
-    # Try normal download
-    if ! wget -nc "$url"; then
-        echo "X Failed to download from official server."
-        echo "-> Trying to find it on the Wayback Machine…"
-
-        # Query Wayback Machine API to find latest snapshot
-        wayback_info=$(curl -s "https://archive.org/wayback/available?url=$url")
-        wayback_url=$(printf "%s" "$wayback_info" | grep -oP '"url":"\K[^"]+')
-
-        if [ -n "$wayback_url" ]; then
-            echo "-> Snapshot found: $wayback_url"
-            if ! wget -nc "$wayback_url" -O "$file"; then
-                echo "X Failed to download even via Wayback."
-                return 1
-            fi
-        else
-            echo "X No snapshot found on the Wayback Machine."
-            return 1
-        fi
-    fi
-
-    # Extraction and build
-    tar -xf "$file"
-    cd "$package_name-$version"
-    ./configure --prefix="$install_dir"
-    make
-    make install
-    cd ..
+	wget -nc "https://ftp.gnu.org/gnu/$package_name/$package_name-$version.tar.gz"
+	tar -xf "$package_name-$version.tar.gz"
+	cd "$package_name-$version"
+	./configure --prefix="$install_dir"
+	make
+	make install
+	cd ..
 }
 
 
